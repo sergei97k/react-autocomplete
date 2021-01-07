a79c6c90abd2dd48985a150721cbbb974f16f79a
Serhii Kononov <sergei97k@gmail.com>
2021-01-07 22:24:40 +0200

    added SCSS support and rewrote Autocomplete styles

--- src/components/Autocomplete/Autocomplete.js
+++ src/components/Autocomplete/Autocomplete.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import "./styles.css";
+import "./Autocomplete.scss";
 
 const Autocomplete = ({ value, options, onChange }) => (
   <div>
